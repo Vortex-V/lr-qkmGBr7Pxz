0a1a1312fdb70ca89ed6cd841e163d215acdce18
Vitaliy <vitalik124124@mail.ru>
2024-06-03 19:57:02 +0300

    tkp_3

--- tkp/3/assets/js/app/App.js
+++ tkp/3/assets/js/app/App.js
@@ -1,49 +1,65 @@
 import * as THREE from 'three';
 import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
+import {models} from '../../models/models';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
+const camera = new THREE.PerspectiveCamera(
+    60, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({alpha: true});
 
 const controls = new OrbitControls(camera, renderer.domElement);
 const loader = new GLTFLoader();
 
-const models = {
-    floppa: 'floppa_but_bad',
-    forestGod: 'princess_mononoke_-_forest_god',
-    arny: 'the_head_of_the_sculpture_use_zbrush',
-    tinker: 'the_thinker_by_auguste_rodin'
-};
 
 let App = function (config) {
 
     this.config = Object.assign({
         container: document.getElementById('app'),
-        lights: true,
+        lights: {
+            show: true,
+            config: {
+                forward: true,
+                backward: true,
+                right: true,
+                left: true,
+                bottom: true,
+            }
+        },
         plain: true,
-        model: true,
-        modelName: 'floppa',
+        model: {
+            show: true,
+            name: 'floppa',
+            type: 'gltf'
+        },
+        resolution: 1,
+        rotating: false,
     }, config);
+    config = undefined;
 
-    const container = config.container;
-    renderer.setSize(window.innerWidth, window.innerHeight);
+    const container = this.config.container;
+    renderer.setSize(
+        window.innerWidth * this.config.resolution,
+        window.innerHeight * this.config.resolution, false);
     container.appendChild(renderer.domElement);
 
-    function loadModel(modelName) {
-        loader.load(`assets/models/${models[modelName]}/scene.gltf`, function (gltf) {
+    function loadModel(model) {
+        let source;
+        switch (model.type) {
+            case 'gltf':
+                source = `${models[model.name]}/scene.gltf`;
+                break;
+            case 'gld':
+                source = models[model.name];
+                break;
+            default:
+                source = `${models[model.name]}/scene.gltf`
+        }
+        loader.load(`assets/models/${source}`, function (gltf) {
             scene.add(gltf.scene);
         }, undefined, function (error) {
             console.error(error);
         });
-    }
-
-    function animate() {
-        requestAnimationFrame(animate);
-
-        controls.update();
-
-        renderer.render(scene, camera);
     }
 
     function loadPlain() {
@@ -64,33 +80,50 @@ let App = function (config) {
         return light;
     }
 
-    function loadLights() {
-        scene
-            .add(createLight({x: 0, y: 10, z: 10}))
-            .add(createLight({x: 10, y: 10, z: 0}))
-            .add(createLight({x: -10, y: 10, z: 0}))
-            .add(createLight({x: 0, y: 10, z: -10}));
+    function loadLights(lights) {
+        const config = {
+            forward: {x: 0, y: 10, z: 10}, // forward
+            backward: {x: 0, y: 10, z: -10}, // backward
+            right: {x: 10, y: 10, z: 0}, // right
+            left: {x: -10, y: 10, z: 0}, // left
+            bottom: {x: 0, y: -10, z: 0} // bottom
+        };
+
+        for (const [name, show] of Object.entries(lights.config)) {
+            const position = config[name] || undefined;
+            position !== undefined && show && scene.add(createLight(position));
+        }
+    }
+
+    function runRotating() {
+        controls.autoRotate = true;
+        controls.autoRotateSpeed = 5;
     }
 
     this.run = function () {
-        if (this.config.plain) {
-            loadPlain();
-        }
-        if (this.config.lights) {
-            loadLights();
-        }
-        if (this.config.model) {
-            loadModel(this.config.modelName);
-        }
+        const config = this.config;
+        config.plain && loadPlain();
+        config.lights.show && loadLights(config.lights);
+        config.model.show && loadModel(config.model);
+        config.rotating && runRotating();
 
-        camera.position.set(0, 5, 10);
-        controls.update();
+        camera.position.set(1, 1, 1);
 
         renderer.setAnimationLoop(() => {
-            animate();
+            controls.update();
+            renderer.render(scene, camera);
         });
 
     }
+
+    function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+
+        renderer.setSize(window.innerWidth, window.innerHeight)
+    }
+
+    window.addEventListener('resize', onWindowResize)
 }
 
 export default App;
--- tkp/3/assets/js/main.js
+++ tkp/3/assets/js/main.js
@@ -1,12 +1,33 @@
 import App from './app/App'
 import WebGL from 'three/addons/capabilities/WebGL.js';
 
-const appContainer = document.getElementById('app');
-
 if (WebGL.isWebGLAvailable()) {
+
+    const appContainer = document.getElementById('app');
+
+    const urlParams = new URLSearchParams(window.location.search);
+    let model = urlParams.get('model');
+    model = model === null ? 'kv2' : model;
+
     window.app = new App({
         container: appContainer,
-        model: false
+        plain: false,
+        lights: {
+            show: true,
+            config: {
+                forward: 1,
+                backward: 0,
+                right: 1,
+                left: 1,
+                bottom: 0,
+            }
+        },
+        model: {
+            show: true,
+            name: model,
+        },
+        resolution: 0.8,
+        rotating: 0,
     });
     app.run();
 } else {
