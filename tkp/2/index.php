d7c21ba3695b3ea54ad2448dd68d22fdf5b61937
Vitaliy <vitalik124124@mail.ru>
2024-06-12 18:54:10 +0300

    tkp_2

--- tkp/2/index.php
+++ tkp/2/index.php
@@ -5,6 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Домашняя страничка</title>
     <style>
+        /* Стили тега body
+        Установлен внешний отступ 0,
+        отображение flex с позиционированием по центру,
+        высота 100% от высоты экрана (100vh),
+        шрифты без засечек.
+        */
         body {
             margin: 0;
             overflow: hidden;
@@ -13,13 +19,16 @@
             align-items: center;
             height: 100vh;
             color: white;
-            background-color: #474242
-            position: relative
+            background-color: #474242;
+            position: relative;
 
             font-family: sans-serif;
             font-size: 14px;
         }
 
+        /* Стили фонового блока с шейдером
+        Фискированная позиция позади всего контента на всю ширину и высоту экрана
+        */
         .shader-background {
             position: fixed;
             width: 100vw;
@@ -32,10 +41,12 @@
             min-height: 100%;
         }
 
+        /* Стили блока с контентом
+        Отображение в режиме flex столбцом с центровкой содержимого блока,
+        отображение поверх всего контента страницы,
+        задний фон тёмный полупрозрачный, скругление углов блока
+        */
         .content {
-            position: relative;
-
-            /* flex align */
             display: flex;
             flex-direction: column;
             justify-content: center;
@@ -51,7 +62,8 @@
 </head>
 <body>
 <div class="shader-background">
-    <iframe width="640" height="360" frameborder="0"
+    <!-- Фрейм с шейдером -->
+    <iframe width="640" height="360"
             src="https://www.shadertoy.com/embed/3tBGRm?gui=false&t=10&paused=false&muted=false" allowfullscreen></iframe>
 </div>
 
@@ -60,38 +72,41 @@
         <h1>Здравствуйте, товарищи!</h1>
     </header>
     <main>
-        <section id="about">
+        <div id="about">
             <h2>О себе</h2>
             <p>
-                <?= "Александров Виталий Евгеньевич.<br>" ?>
+                <?= "Александров Виталий Евгеньевич.<br>" // Вывод текста ?>
                 <span id="additional-bio"></span>
             </p>
             <?php
+             // Получение версии PHP и вывод текста
             $phpversion = phpversion();
             echo "Страница работает на PHP $phpversion";
             ?>
-        </section>
+        </div>
 
-        <section id="video">
+        <div id="video">
             <h2>Продукт отечественной рок музыки</h2>
             <h3>Radio Tapok - Петропавловск</h3>
+             <!-- Фрейм с видеоплеером -->
             <?=
             '<iframe width="560" height="315" src="https://www.youtube.com/embed/Lwf9ACmduHM?si=5dvrtcz-cTHis6od"
-                    title="YouTube video player" frameborder="0"
+                    title="YouTube video player"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                     allowfullscreen></iframe>'
             ?>
-        </section>
+        </div>
 
-        <section id="music">
+        <div id="music">
             <h3>Radio Tapok - Ермак</h3>
+            <!-- Аудиоплеер -->
             <?=
             '<audio controls>
                 <source src="media/tapok_ermak.mp3" type="audio/mp3">
                 Ваш браузер не поддерживает аудиоэлемент.
             </audio>'
             ?>
-        </section>
+        </div>
     </main>
 </div>
 
